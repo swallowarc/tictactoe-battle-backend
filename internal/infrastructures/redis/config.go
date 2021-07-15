9d7db4b96575c9fd3617132dabfddac126eae7e9
mjkt39 <k.miyaji@swallowarc.jp>
2021-07-15 10:55:25 +0900

    fix redis default password

--- internal/infrastructures/redis/config.go
+++ internal/infrastructures/redis/config.go
@@ -4,6 +4,6 @@ package redis
 // Redis settings.
 type Config struct {
 	HostPort string `envconfig:"host_port" default:"localhost:6379"`
-	Password string `envconfig:"password" default:"password"`
+	Password string `envconfig:"password" default:""`
 	DB       int    `envconfig:"db" default:"0"`
 }
